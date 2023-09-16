12a9ac818910bb09dd458f87ed9c19fa46bf6620
beerbank3 <yg050140528@gmail.com>
2023-09-16 22:45:19 +0900

    도커 추가로인한 코드 수정

--- app/api/endpoints/login.py
+++ app/api/endpoints/login.py
@@ -1,28 +1,28 @@
 from fastapi import APIRouter, HTTPException, Depends, status
 from sqlalchemy.orm import Session
-from models.user import User
-from db.database import get_db
-from schemas.user import UserLogin
-from crud.crud_user import CRUDUser
-from core.security import create_access_token
-import schemas.token
+from app.models.model import User
+from app.db.database import get_db
+from app.schemas.user import UserLogin
+from app.crud.crud_user import CRUDUser
+from app.core.security import create_access_token
+import app.schemas.token as schemas
+from app.core.security import api_key_header, delete_token
 
 router = APIRouter()
 
-@router.post("/login/", response_model=schemas.token.Token)
+@router.post("/login/", response_model=schemas.Token)
 def login(user_login: UserLogin, db: Session = Depends(get_db)):
-    print(user_login)
     db_user = CRUDUser.authenticate(db,email=user_login.email, password=user_login.password)
 
     if db_user is None:
         raise HTTPException(status_code=400, detail="Invalid credentials")
 
-    access_token = create_access_token(db_user.email)
+    access_token = create_access_token(db_user.id)
     
     return {"access_token": access_token, "token_type": "bearer"}
 
 
-@router.post("/logout/", status_code=status.HTTP_204_NO_CONTENT)
-def logout():
-    
-    pass
+@router.post("/logout/")
+def logout(token: str = Depends(api_key_header)):
+    delete_token(token)
+    return {"message":"로그아웃"}

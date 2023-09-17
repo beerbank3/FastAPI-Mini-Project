33103133d6556cc564b41e970a875e0bbb92ba7b
beerbank3 <yg050140528@gmail.com>
2023-09-17 22:37:45 +0900

    schemas.user is_active 제거

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -4,7 +4,6 @@ from pydantic import BaseModel, EmailStr
 
 class UserBase(BaseModel):
     email: Optional[EmailStr] = None
-    is_active: Optional[bool] = True
     full_name: Optional[str] = None
 
 
